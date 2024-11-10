59dd60e9b8eae7dd802b3057cd3779776f70605d
votik-admin <mmpl@votik.app>
2024-11-10 17:49:14 +0530

    added dashboard redirect

--- app/(organizers)/organizer/dashboard/page.tsx
+++ app/(organizers)/organizer/dashboard/page.tsx
@@ -2,11 +2,17 @@ import { getSessionAndOrganizer } from "@app/lib/auth";
 import { redirect } from "next/navigation";
 import React from "react";
 import DashboardPage from "./DashboardPage";
+import { headers } from "next/headers";
 
 const HomePage = async () => {
   const { session, organizer, error } = await getSessionAndOrganizer();
+
   if (!session || !organizer || error) {
-    redirect("/auth/login");
+    const headersList = headers();
+    const header_url = headersList.get("x-url") || "";
+    const path = new URL(header_url).pathname;
+    if (error || !session) redirect(`/auth/login?redirect=${path}`);
+    if (!organizer) redirect(`/organizer/signup?redirect=${path}`);
   }
 
   return (
